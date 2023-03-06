3cbfee6f1882feff7b3d7f7935b2de2d01668898
eoghankbdev <eoghankb@gmail.com>
2023-03-06 14:36:05 +0000

    add x-layout for grid componenet

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,36 +1,34 @@
 @extends ('components.layout')
-
 @section ('content')
 @include('posts-header')
     {{-- @foreach ($posts as $post)
-    <article class="{{ $loop->even ? 'mb-6' : '' }}">
-        <h1 class="font-bold text-3xl mb-10 mt-10">
-            <a href="/posts/{{ $post->slug }}">
-                {{ $post->title }}
-            </a>
-        </h1>
-        <p class="font-bold">
-            By <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
-        </p>
-        <div>
-            {{ $post->excerpt }}
-        </div>
-    </article>
-    @endforeach --}}
-    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
-            <x-post-featured-card />
+        <article class="{{ $loop->even ? 'mb-6' : '' }}">
+            <h1 class="font-bold text-3xl mb-10 mt-10">
+                <a href="/posts/{{ $post->slug }}">
+                    {{ $post->title }}
+                </a>
+            </h1>
+            <p class="font-bold">
+                By <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
+            </p>
+            <div>
+                {{ $post->excerpt }}
+            </div>
+        </article>
+        @endforeach --}}
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6"><a href=""></a>
+        @if ($posts->count())
+            <x-posts-grid :posts="$posts" />
+        @else 
+            <p class="text-center">No Posts yet. Please check back later.</p>
+        @endif
 
-        <div class="lg:grid lg:grid-cols-2">
-            <!--Grid with two blog posts-->
-            <x-post-card />
-            <x-post-card />
-        </div>
 
-        <div class="lg:grid lg:grid-cols-3">
+        {{-- <div class="lg:grid lg:grid-cols-3">
             <!--Grid with three blog posts-->
             <x-post-card />
             <x-post-card />
             <x-post-card />
-        </div>
+        </div> --}}
     </main>
 @endsection
