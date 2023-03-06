14c7b60e9f3df9b501d0e5044178a2c26af08dfc
eoghankbdev <eoghankb@gmail.com>
2023-03-06 15:46:53 +0000

    removed dot in front of / to load images

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -33,7 +33,7 @@
         <nav class="md:flex md:justify-between md:items-center">
             <div>
                 <a href="/">
-                    <img src="./images/logo.svg" alt="Laracasts Logo" width="165" height="16">
+                    <img src="/images/logo.svg" alt="Laracasts Logo" width="165" height="16">
                 </a>
             </div>
 
