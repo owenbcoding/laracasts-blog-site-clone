355ecaa3ed304084bf382e23295b1301a0549d34
eoghankbdev <eoghankb@gmail.com>
2023-03-07 10:40:36 +0000

    created dropdown component

---
+++ resources/views/components/dropdown.blade.php
@@ -0,0 +1,12 @@
+@props(['trigger'])
+
+<div x-data="{ show: false }" @click.away="show = false">
+    {{-- Trigger --}}
+    <div @click="show = ! show">
+        {{ $trigger }}
+    </div>
+    {{-- Dropdown Links --}}
+    <div x-show="show" class="py-2 absolute bg-gray-100 mt-2 rounded-xl w-32 z-50" style="display: none">
+        {{ $slot }}
+    </div>
+</div>
