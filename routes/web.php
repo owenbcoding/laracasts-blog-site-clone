1e3c44ec2cf07cbab5e7d43d859a1799e5426ad6
eoghankbdev <eoghankb@gmail.com>
2023-03-07 10:41:02 +0000

    add currentCategory to route categories

--- routes/web.php
+++ routes/web.php
@@ -36,12 +36,12 @@ Route::get('/posts/{post:slug}', function (Post $post) {
     return view('post', [
         'post' => $post 
     ]);
-
 });
 
 Route::get('categories/{category:slug}', function (Category $category) {
     return view('posts', [
         'posts' => $category->posts,
+        'currentCategory' => $category,
         'categories' => Category::all()
      ]);
 });
