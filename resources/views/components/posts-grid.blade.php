eae41174cfc96abc32cfb6b3279de69a7c2530a1
eoghankbdev <eoghankb@gmail.com>
2023-03-09 23:55:12 +0000

    made into a single line

--- resources/views/components/posts-grid.blade.php
+++ resources/views/components/posts-grid.blade.php
@@ -1,16 +1,13 @@
 @props(['posts'])
 
 <x-post-featured-card :post="$posts[0]" />
+
     @if ($posts->count() > 1)
         <div class="lg:grid lg:grid-cols-6">
             <!--Grid with two blog posts-->
             @foreach ($posts->skip(1) as $post)
-                {{-- @dd($loop) --}}
-                <x-post-card
-                    :post="$post"
-                    class="{{ $loop->iteration < 3 ? 'col-span-3' : 'col-span-2' }}"
-                />
+                <x-post-card :post="$post" class="{{ $loop->iteration < 3 ? 'col-span-3' : 'col-span-2' }}" />
+                {{-- {{ $post->slug }} --}}
             @endforeach
-            {{-- <x-post-card /> --}}
         </div>
     @endif
