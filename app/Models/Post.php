e3849a5c70156adbc4b7ca715a9a4678809e7cbd
eoghankbdev <eoghankb@gmail.com>
2023-03-10 18:31:33 +0000

    created query for categories

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -14,6 +14,19 @@ class Post extends Model
 
     protected $with = ['category', 'author'];
 
+    public function scopeFilter($query, array $filters) // Post::newQuery()->filter()->where('title, '')
+    {
+        $query->when($filters['search'] ?? false, fn ($query, $search) => 
+            $query 
+                ->where('title', 'like', '%' . request('search') . '%')
+                ->orWhere('body', 'like', '%' . request('search') . '%'));
+
+        $query->when($filters['category'] ?? false, fn ($query, $category) => 
+            $query->whereHas('category', fn ($query) => 
+                $query->where('slug', $category))
+        );
+    }
+
     public function category()
     {
         // hasone, hasmany, belongsTo, blongstoMany
