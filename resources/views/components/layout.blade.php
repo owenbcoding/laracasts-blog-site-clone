f594c6b1c093a48d4756df8c54c356b07b25c429
eoghankbdev <eoghankb@gmail.com>
2023-03-10 18:45:37 +0000

    removed commented out old code

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,26 +1,3 @@
-{{-- <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Example Blog</title>
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
-    @vite('resources/css/app.css')
-
-    <link rel="stylesheet" href="/app.css">
-    <script src="/app.js"></script>
-</head>
- @yield('content')
-    {{ $slot }}
-<body>
-</body>
-</html> --}}
-
 <!doctype html>
 
 <title>Laravel From Scratch Blog</title>
